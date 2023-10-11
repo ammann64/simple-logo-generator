77767d01466f86d0cd883aaac8047c80c4373f02
ammann64 <austinmmann64@gmail.com>
2023-10-10 20:49:36 -0500

    Add inquirer framework and question objects for use of prompting user

---
+++ index.js
@@ -0,0 +1,28 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
+const { default: inquirer } = require("inquirer");
+const jest = require("jest");
+const questions = [
+    {name: 'text', message: 'Please enter up to three characters to display in the logo: '},
+    {name: 'textColor', message: 'Please enter the desired text color in keyword or hexidecimal format: '},
+    {type: 'list', name: 'shape', message: 'Please choose a shape: ', choices: ['circle', 'triangle', 'square']},
+    {name: 'shapeColor', message: 'Please enter the desired shape color in keyword or hexidecimal format: '}
+];
+
+
+inquirer
+    .prompt([
+
+    ])
