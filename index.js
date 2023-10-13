26a34ac93ef0a5a69533bd5f7b6d691542cedfd5
ammann64 <austinmmann64@gmail.com>
2023-10-13 15:48:29 -0500

    Add functions to shape classes to format the svg

--- index.js
+++ index.js
@@ -12,9 +12,11 @@
 // AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
-const { default: inquirer } = require("inquirer");
+const inquirer = require('./node_modules/inquirer/');
 const jest = require("jest");
 const shapes = require('./lib/shapes');
+const fs = require('fs');
+const { error } = require("console");
 
 const questions = [ // Questions array to pass into inquirer
     {name: 'text', message: 'Please enter up to three characters to display in the logo: '},
@@ -29,4 +31,28 @@ inquirer
     .prompt(questions) //Prompts the user
     .then((answers) => { //Once answers are received...
         const {text, textColor, shape, shapeColor} = answers;
+        switch (shape) {
+            case 'circle':
+                var logoData = new shapes.Circle(text, textColor, shapeColor);
+                logo = logoData.formatCircle();
+                RenderSVG(logo);
+                break;
+            case 'rectangle':
+                var logoData = new shapes.Rectangle(text, textColor, shapeColor);
+                logo = logoData.formatRectangle();
+                RenderSVG(logo);
+                break;
+            case 'triangle':
+                var logoData = new shapes.Triangle(text, textColor, shapeColor);
+                logo = logoData.formatTriangle();
+                RenderSVG(logo);
+                break;
+            default:
+                console.error('Invalid shape, please try again');
+        }
     })
+
+
+function RenderSVG(logo) {
+
+}
